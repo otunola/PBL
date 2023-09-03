e35173a284e3d68d834e64cd416b424d48d6e9bb
“otun” <otunolaaws@gmail.com>
2023-09-03 14:09:31 +0100

    edit

--- main.tf
+++ main.tf
@@ -7,6 +7,4 @@ resource "aws_vpc" "main" {
   cidr_block                     = "172.16.0.0/16"
   enable_dns_support             = "true"
   enable_dns_hostnames           = "true"
-  enable_classiclink             = "false"
-  enable_classiclink_dns_support = "false"
 }
